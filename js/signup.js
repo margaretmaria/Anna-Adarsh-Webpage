d5dc354f2f2c49cf610d22f67dae811953af57a4
margaret <margaretmaria@Margarets-MacBook-Pro.local>
2014-08-29 14:39:53 +0530

    Changes in javascript working

---
+++ js/signup.js
@@ -0,0 +1,40 @@
+function signup(){
+	/*var firstname = document.getElementsByName('first_name')[0].value;
+	var lastname = document.getElementsByName('last_name')[0].value;
+	var username = document.getElementsByName('user_name')[0].value;
+	var password = document.getElementsByName('password')[0].value;
+	var cpassword = document.getElementsByName('cpassword')[0].value;
+
+	if (username=="") {
+		alert("User doesn't mathc");
+	}else if(password==cpassword){
+		userDetail.firstname = firstname;
+		userDetail.lastname = lastname;
+		userDetail.username = username;
+		userDetail.password = password;
+	}else{
+		alert("Password doesn't mathc");
+	}
+console.log(userDetail);
+console.log(userDetails);*/
+
+
+test++;
+console.log(test);
+}
+
+var test = 0;
+
+
+/*var userDetails {
+	userDetail[]
+}*/
+
+var userDetail = {
+firstname : "",
+lastname : "",
+username : "",
+password : "",
+cpassword : ""
+
+}
